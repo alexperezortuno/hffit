58d6bdc08026c0810ebc9d15d5527342cda20dc0
alexperezortuno <alexperezortuno@gmail.com>
2026-08-17 21:37:35 -0400

    Add detailed model parameters and refactor `CompatibilityResult` for better readability and maintainability.

--- internal/domain/model.go
+++ internal/domain/model.go
@@ -7,6 +7,13 @@ type Model struct {
 	Parameters   uint64
 	Architecture string
 	ModelType    string
+
+	HiddenSize            int
+	HiddenLayers          int
+	AttentionHeads        int
+	KeyValueHeads         int
+	MaxPositionEmbeddings int
+	HeadDim               int
 }
 
 type Precision string
@@ -29,17 +36,43 @@ const (
 )
 
 type CompatibilityResult struct {
-	Precision         Precision
-	RequiredVRAM      uint64
-	AvailableVRAM     uint64
-	AvailableRAM      uint64
-	Score             int
-	Level             CompatibilityLevel
+	Precision Precision
+
+	WeightsBytes  uint64
+	KVCacheBytes  uint64
+	OverheadBytes uint64
+	RequiredVRAM  uint64
+
+	AvailableVRAM uint64
+	AvailableRAM  uint64
+
+	ContextSize int
+
+	Score int
+	Level CompatibilityLevel
+
 	CanFitVRAM        bool
 	CanFitWithOffload bool
-	Message           string
+
+	Message string
 }
 
 func (r CompatibilityResult) RequiredVRAMGB() float64 {
-	return float64(r.RequiredVRAM) / (1024 * 1024 * 1024)
+	return BytesToGB(r.RequiredVRAM)
+}
+
+func (r CompatibilityResult) WeightsGB() float64 {
+	return BytesToGB(r.WeightsBytes)
+}
+
+func (r CompatibilityResult) KVCacheGB() float64 {
+	return BytesToGB(r.KVCacheBytes)
+}
+
+func (r CompatibilityResult) OverheadGB() float64 {
+	return BytesToGB(r.OverheadBytes)
+}
+
+func BytesToGB(value uint64) float64 {
+	return float64(value) / (1024 * 1024 * 1024)
 }
